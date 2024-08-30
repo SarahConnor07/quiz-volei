13b85d53c510ea0d8be283299f05d0314368d45e
Sarah <117759674+SarahConnor07@users.noreply.github.com>
2024-08-29 21:36:08 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é a sua posição favorita em quadra?",
+        alternativas: [
+            {
+                texto: "A) Levantador",
+                afirmacao: [
+                    "Sua paixão está em atacar e marcar pontos. Você vive para aqueles momentos de explosão e adora sentir a adrenalina ao fazer uma grande jogada."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B)Ponta 'ou Passador'",
+                afirmacao: [
+                    "Para você, o jogo é ganho na defesa. Você é a linha de frente na recepção e adora impedir os ataques adversários com defesas incríveis."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você se prepara para uma partida?",
+        alternativas: [
+            {
+                texto: "A)Aquecimento leve e foco na estratégia do time",
+                afirmacao: [
+                    "Seu papel é bloquear e intimidar os adversários na rede. Você é o escudo do time, focado em parar os ataques com bloqueios precisos e saltos poderosos.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B)Foco em exercícios de concentração e mentalidade",
+                afirmacao: [
+                    "Você é o cérebro do time, sempre organizando as jogadas e garantindo que cada ataque seja bem construído. Seu foco é na estratégia e no controle do jogo."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "O que te motiva a jogar vôlei??",
+        alternativas: [
+            {
+                texto: "A)A adrenalina de marcar pontos",
+                afirmacao: [
+                    "Sua paixão está em atacar e marcar pontos. Você vive para aqueles momentos de explosão e adora sentir a adrenalina ao fazer uma grande jogada."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "B)A importância de cada defesa e passe",
+                afirmacao: [
+                    "Seu papel é bloquear e intimidar os adversários na rede. Você é o escudo do time, focado em parar os ataques com bloqueios precisos e saltos poderosos."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você reage após perder um set difícil?",
+        alternativas: [
+            {
+                texto: "A) Mantenho a calma e foco na estratégia para o próximo set",
+                afirmacao: [
+                    "Você é o cérebro do time, sempre organizando as jogadas e garantindo que cada ataque seja bem construído. Seu foco é na estratégia e no controle do jogo."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "B)Converso com a equipe para ajustar bloqueios e cobertura",
+                afirmacao: [
+                    "Seu papel é bloquear e intimidar os adversários na rede. Você é o escudo do time, focado em parar os ataques com bloqueios precisos e saltos poderosos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a sua estratégia de jogo ideal?",
+        alternativas: [
+            {
+                texto: "A) Controlar o ritmo do jogo com levantamentos precisos",
+                afirmacao: [
+                    "Para você, o jogo é ganho na defesa. Você é a linha de frente na recepção e adora impedir os ataques adversários com defesas incríveis."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "B) Forçar o ataque para quebrar a defesa adversária",
+                afirmacao: [
+                    "Sua paixão está em atacar e marcar pontos. Você vive para aqueles momentos de explosão e adora sentir a adrenalina ao fazer uma grande jogada."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o seu maior desafio em uma partida de vôlei?",
+        alternativas: [
+            {
+                texto: "A)Fazer passes perfeitos sob pressão",
+                afirmacao: [
+                    "Para você, o jogo é ganho na defesa. Você é a linha de frente na recepção e adora impedir os ataques adversários com defesas incríveis."
+                ],
+            },
+            {
+                texto: "B)Ajustar o bloqueio para cada tipo de ataque",
+                afirmacao: [
+                    "Você é o cérebro do time, sempre organizando as jogadas e garantindo que cada ataque seja bem construído. Seu foco é na estratégia e no controle do jogo."
+                ],
+            },
+        ]
+    },
+];
